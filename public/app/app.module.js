97d0c6e48f410c7e12a47bd2572810d7e3b7a5d5
Warren Wong <wrrnwng@gmail.com>
2015-11-01 01:18:50 -0700

    refactor to use angular material layout attr

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,6 +1,5 @@
 angular.module('BooksApp', ['menu', 'welcome', 'books', 'lists'])
   .controller('mainController', ['$scope', function($scope) {
-    $scope.layout = window.innerWidth < 480 ? 'column' : 'row';
     $scope.header = 'My books';
   }])
   .directive('main', function() {
